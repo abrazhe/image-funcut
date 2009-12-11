7d3bca60185dfcda3567ef6fef7bb97351a23dbd
Alexey Brazhe <brazhe@gmail.com>
2009-12-11 22:50:01 +0300

    correction in setup.py

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from distutils.core import setup
 
 setup(name='image-funcut',
       version = '0.0.1a',
-      py_modules = ['ifuncut', 'readleicaxml'],
+      py_modules = ['ifuncut', 'readleicaxml'],)
