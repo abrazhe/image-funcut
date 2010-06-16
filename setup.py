e5cbe99777c926d06f8d3a73852c0b709477ff69
Alexey Brazhe <abrazhe@users.noreply.github.com>
2010-06-16 19:26:54 +0200

    Moved ifuncut to imfun/interactive

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
 setup(name='image-funcut',
-      version = '0.0.4',
-      py_modules = ['ifuncut',
+      version = '0.0.5',
+      py_modules = ['imfun.interactive',
                     'imfun.opt',
                     'imfun.readleicaxml',
                     'imfun.aux_utils',
