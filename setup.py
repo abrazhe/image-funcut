49e1bb39d3e4b2851c43f10413f396b6b06bc7d3
Alexey Brazhe <brazhe@gmail.com>
2010-08-19 20:16:49 +0200

    pica is now a module in imfun

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup
 setup(name='image-funcut',
       version = '0.0.5',
       py_modules = ['imfun.ui',
+                    'imfun.pica',
                     'imfun.opt',
                     'imfun.readleicaxml',
                     'imfun.aux',
