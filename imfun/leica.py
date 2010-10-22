c6d7130effde096c0fdfec3887822c3afeff646c
Alexey Brazhe <brazhe@gmail.com>
2010-10-22 22:38:31 +0400

    1) now leicaprops only depends on built-in python xml facilities; 2) added win-install.bat

--- imfun/leica.py
+++ imfun/leica.py
@@ -28,7 +28,7 @@ def low_high_pair(a):
     return map(long, (get_prop(a, "LowInteger"),
                       get_prop(a, "HighInteger")))
 
-class LeicaProps:
+class LeicaProps_old:
     def __init__(self,xmlfilename):
         import libxml2
         doc = libxml2.parseFile(get_xmljob(xmlfilename))
@@ -53,9 +53,65 @@ class LeicaProps:
         self.stop_time = ticks_to_ms(*low_high_pair(tstamps[-1]))
         
         
+# ==============
+
+#from xml.sax import saxutils
+import xml.sax
+from xml.sax import make_parser
+from xml.sax.handler import feature_namespaces
+
+class FindVal(xml.sax.ContentHandler):
+    names = ['ScannerSettingRecord', 'DimensionDescription', 'TimeStamp']
+    def __init__(self, header, value):
+        self.search_title, self.search_value = header, value
+        self.res = {}
+        self.timestamps = []
+    def startElement(self, name, attrs):
+        # If it's not a needed element, ignore it
+        if name not in self.names : return
+        title = attrs.get('Identifier', None)
+        value = attrs.get('Variant', None)
+        if title == 'nDelayTime_ms':
+            self.res['dt'] = float(value)/1e3
+        elif name == 'DimensionDescription':
+            dimid = attrs.get('DimID')
+            num_elem = float(attrs.get('NumberOfElements'))
+            length = float(attrs.get('Length'))*1e6
+            unit = attrs.get('Unit',None)
+            if unit == 'm':
+                if dimid == '1':
+                    self.res['xdim'] = length
+                    self.res['dx'] = length/num_elem
+                elif dimid == '2':
+                    self.res['ydim'] = length
+                    self.res['dy'] = length/num_elem
+        elif name == 'TimeStamp':
+            high = long(attrs.get('HighInteger',None))
+            low = long(attrs.get('LowInteger',None))
+            self.timestamps.append(ticks_to_ms(low,high)/1000)
+    def endDocument(self):
+        self.res['start_time'] = self.timestamps[0]
+        self.res['stop_time'] = self.timestamps[-1]
+        #print self.res
             
-            
-            
+
+def leica_parser():
+    parser = make_parser()
+    parser.setFeature(feature_namespaces, 0)
+    dh = FindVal('nDelayTime_ms', '62')
+    parser.setContentHandler(dh)
+    return parser
+
+
+class LeicaProps():
+    def __init__(self, xmlfilename):
+        p = leica_parser()
+        fname = get_xmljob(xmlfilename)
+        p.parse(fname)
+        res = p.getContentHandler().res
+        self.__dict__.update(res)
+        
+        
             
                 
         
