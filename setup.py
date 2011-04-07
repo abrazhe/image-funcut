4b74cb3e89e9ce7859accf7c56ce0ba6d65f4b5d
Alexey Brazhe <brazhe@gmail.com>
2011-04-07 20:42:22 +0400

    Modifications/fixes in fseq, new minor release in setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='image-funcut',
-      version = '0.0.7',
+      version = '0.0.9',
       scripts = ['imfun/frame_viewer.py'],
       requires = ['swan'],
       py_modules = ['imfun.ui',
