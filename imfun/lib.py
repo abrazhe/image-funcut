94a3ffe8bca36d77f33a8938f38e3bba41ffd1fd
Alexey Brazhe <brazhe@gmail.com>
2011-06-17 19:40:17 +0400

    Added som_cluster_fseq to lib.py and cluster_map_permutation to som.py

--- imfun/lib.py
+++ imfun/lib.py
@@ -431,3 +431,16 @@ def read_lasaf_txt(fname):
         print "%s: Exception"%fname, type(inst)
         return None
 
+
+
+def som_cluster_fseq(seq, **kwargs):
+	from itertools import product
+	from imfun import som
+	shape = seq.shape()
+	a = seq.as3darray()
+	tracks = np.array([a[:,i,j] for i,j in
+			   product(*map(xrange(shape)))])
+	perm = np.permutation(np.product(shape))
+	affiliations = som.som1(seq,**kwargs)
+	return som.cluster_map_permutation(affiliations, perm, shape)
+	
