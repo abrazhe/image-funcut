fc8a04a9bc1023e7f0f1b301841b6e6ee38a8c1c
Alexey Brazhe <brazhe@gmail.com>
2014-09-01 11:00:01 +0200

    improving tracking routines

--- imfun/track.py
+++ imfun/track.py
@@ -1,8 +1,9 @@
 import lib
 import numpy as np
-from scipy.interpolate import splrep,splev
-from scipy.signal import cspline1d
+import scipy.interpolate as ip
 
+
+import atrous
 
 def limit_bounds(vec, lbound, ubound):
    out = np.copy(vec)
@@ -11,6 +12,9 @@ def limit_bounds(vec, lbound, ubound):
    if ubound:
         out = out[out <= ubound]
    return out
+
+def gaussian(mu, sigma):
+    return lambda _x: np.exp(-(_x-mu)**2/(2*sigma**2))
 
 
 def track1(seq, seed, memlen = 5, 
@@ -32,13 +36,13 @@ def track1(seq, seed, memlen = 5,
             out.append((k,el[j]))
     return np.array(out)
 
-def spl1st_derivative(ck):
-    L = len(ck)
-    return [0.5*(ck[mirrorpd(k+1,L)] - ck[mirrorpd(k-1,L)])  for k in range(L)]
+## def spl1st_derivative(ck):
+##     L = len(ck)
+##     return [0.5*(ck[mirrorpd(k+1,L)] - ck[mirrorpd(k-1,L)])  for k in range(L)]
 
-def spl2nd_derivative(ck):
-    L = len(ck)
-    return np.array([ck[mirrorpd(k+1,L)] + ck[mirrorpd(k-1,L)] - 2*ck[k] for k in range(L)])
+## def spl2nd_derivative(ck):
+##     L = len(ck)
+##     return np.array([ck[mirrorpd(k+1,L)] + ck[mirrorpd(k-1,L)] - 2*ck[k] for k in range(L)])
 
 def mirrorpd(k, L):
     if 0 <= k < L : return k
@@ -47,21 +51,33 @@ def mirrorpd(k, L):
 
 def locextr(x, mode = 'max', **kwargs):
    "Finds local maxima when mode = 1, local minima when mode = -1"
-   ck = cspline1d(x, 2) #smooth spline coefs
-   d1ck = spl1st_derivative(ck) # first derivative
-   d2ck = spl2nd_derivative(ck) # second derivative
+   sp = ip.UnivariateSpline(np.arange(len(x)),atrous.smooth(x),s=0)
    if mode in ['max', 'min']:
-       tck = splrep(range(len(x)), d1ck, **kwargs)
+       sp = sp.derivative(1)
    elif mode in ['gup', 'gdown', 'gany']:
-       tck = splrep(range(len(x)), d2ck, **kwargs)
+       sp = sp.derivative(2)
    res = 0.05
    xfit = np.arange(0,len(x), res)
-   di = splev(xfit, tck)
+   di = sp(xfit)
    if mode in ['max', 'gup']:
        dersign = np.sign(di)
    elif mode in ['min', 'gdown']:
        dersign = -np.sign(di)
    return xfit[dersign[:-1] - dersign[1:] > 1.5]
+
+def guess_seeds(seq, Nfirst=10):
+    """
+    automatically guess starting seeds for vessel walls
+    relies on the fact that vessel is the largest bright stripe
+    """
+    Nfirst = min(len(seq), Nfirst)
+    y = np.mean(seq[:Nfirst],axis=0)
+    (xfit,yfit), (mx,mn), (gups,gdowns) = lib.extrema2(y, sort_values=True)
+    # highest gradient up to the left of the highest max
+    gu1 = (g for g in gups if g < mx[0]).next()
+    # highest gradient up to the right of the highest max
+    gd1 = (g for g in gdowns if g > mx[0]).next() 
+    return (xfit[gu1], xfit[gd1])
 
 
 def follow_extrema(arr, start, mode='gany', memlen=5):
@@ -78,3 +94,51 @@ def follow_extrema(arr, start, mode='gany', memlen=5):
     extrema = map(_ext, arr)
     v = track1(extrema, start, memlen=memlen)
     return v
+
+
+def track_pkalman(xv,seq, seed_mu,seed_var=4.,gain=0.25):
+    history = seed_mu #seed_mu*np.ones(memlen)
+    delta=0
+    out = []
+    for el in seq:
+        mu = history
+        # broadness of window depends on previous scatter in positions and
+        # inversly on SNR in the current frame (within a window around
+        # predicted position
+        snr = np.max(el)/np.std(el)
+        var = seed_var + delta**2 + (1./snr)**2
+        p = gaussian(mu,var**0.5)(xv)*el
+        z = xv[np.argmax(p)]
+        g = (1+snr)/(1+snr/gain)
+        x = g*mu + (1-g)*z
+        history = x
+        delta = mu-x
+        out.append(x)
+    return np.array(out)
+    
+def v2grads(v):
+    L = len(v)
+    sp2 = ip.UnivariateSpline(np.arange(L),atrous.smooth(v), s=0)
+    xfit = np.arange(0,L,0.1)
+    return np.abs(sp2.derivative(1)(xfit))
+
+def track_walls(linescan,output = 'kalman',Nfirst=None,gain=0.25):
+    '''
+    output can be one of ('kalman',  'extr', 'mean', 'all')
+    '''
+    if Nfirst is None: Nfirst = len(linescan)/2
+    seeds = guess_seeds(linescan,Nfirst)
+    xfit = np.arange(0,linescan.shape[1],0.1)
+    grads = np.array(map(v2grads, linescan))
+    if output in ['kalman', 'mean', 'all']:
+        tk1,tk2 = [track_pkalman(xfit, grads,seed,gain=gain) for seed in seeds]
+    if output in ['extr', 'mean', 'all']:
+        te1,te2 = [follow_extrema(linescan, seed)[:,1] for seed in seeds] 
+    if output == 'mean':
+        return 0.5*(tk1+te1), 0.5*(tk2+te2)
+    elif output == 'kalman':
+        return tk1,tk2
+    elif output == 'extr':
+        return te1,te2
+    elif output == 'all':
+        return tk1,tk2, te1,te2
