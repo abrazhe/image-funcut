13f8198c712ebf60d2eb9a57596741fd084c0240
Alexey Brazhe <brazhe@gmail.com>
2014-09-11 18:54:46 +0200

    added an initial version of Kalman stack filter

--- imfun/filt.py
+++ imfun/filt.py
@@ -156,6 +156,42 @@ def adaptive_medianf2(arr, k=3, s=1):
     d[np.abs(d) > k*ns] = 0 
     return d + approx
     
+
+def kalman_stack_filter(frames, seed='mean', gain=0.5, var=0.05):
+    """Kalman stack filter similar to that of Imagej
+
+    Input:
+    ------
+      - frames: array_like, e.g. nframes x nrows x ncolumns array or list of 2D
+                images
+      - seed: {'mean' | 'first' | 2D array}
+              the seed to start with, defaults to the time-average of all
+              frames, if 'first', then the first frame is used, if 2D array,
+              this array is used as the seed
+      - gain: overall filter gain
+      - var: estimated environment noise variance
+
+    Output:
+    -------
+      - new frames, an nframes x nrows x ncolumns array with filtered frames
+    
+    """
+    if seed is 'mean' or None:
+        seed = np.mean(frames,axis=0)
+    elif seed is 'first':
+        seed = frames[0]
+    out = np.zeros_like(frames)
+    predicted = seed
+    Ek = var*np.ones_like(frames[0])
+    var = Ek
+    for k,M in enumerate(frames):
+        Kk = 1.+ gain*(Ek/(Ek + var)-1)
+        corrected = predicted + Kk*(M-predicted)
+        err = (corrected-predicted)**2/predicted.max()**2 # unclear
+        Ek = Ek*(1.-Kk) + err
+        out[k] = corrected
+        predicted = out[k]
+    return out
     
 
 _bclose = ndimage.binary_closing
