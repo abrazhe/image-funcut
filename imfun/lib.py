7aa6daaca6953e58c8daa54741ee94fa287ecb5d
Alexey Brazhe <alexeybrazhe@yandex.ru>
2014-10-08 21:26:27 +0200

    moved embedding and emedded_to_full from mvm.py to lib.py

--- imfun/lib.py
+++ imfun/lib.py
@@ -228,6 +228,39 @@ def ifnot(a, b):
     if a == None: return b
     else: return a
 
+def embedding(arr, delarrp=True):
+    """Return an *embeding* of the non-zero portion of an array.
+
+    Parameters:
+      - `arr`: array
+      - `delarrp`: predicate whether to delete the `arr`
+
+    Returns tuple ``(out, (sh, slices))`` of:
+        * out: array, which is a bounding box around non-zero elements of an input
+          array
+	* sh:  full shape of the input data
+	* slices: a list of slices which define the bounding box
+    
+    
+    """
+    sh = arr.shape
+    b = np.argwhere(arr)
+    starts, stops = b.min(0), b.max(0)+1
+    slices = [slice(*p) for p in zip(starts, stops)]
+    out =  arr[slices].copy()
+    if delarrp: del arr
+    return out, (sh, slices)
+
+def embedded_to_full(x):
+    """Restore 'full' object from it's *embedding*, 
+    e.g. full image from  object subframe
+    """
+    data, (shape, xslice) = x
+    out = np.zeros(shape, _dtype_)
+    out[xslice] = data
+    return out
+    
+
 def auto_threshold(arr, init_th = None, max_iter = 1e7):
     """
     Automatic threhold with INTERMEANS(I) algorithm
