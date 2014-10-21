5e229098f9fa53ee626408995d611ba871914290
Alexey Brazhe <alexeybrazhe@yandex.ru>
2014-10-21 22:23:50 +0200

    updated som.py, cluster.py, making SOM work faster

--- imfun/cluster.py
+++ imfun/cluster.py
@@ -4,30 +4,31 @@
 ### This has to be re-done using structures for inter-point distances
 import sys
 import numpy as np
-import random as pyrand
+#import random as pyrand
 import itertools as itt
 from scipy import stats
 
 
 ### ---  Distance measures ---
-def minkowsky(p1,p2,k):
+def minkowski(p1,p2,k):
     '''Minkowski distance between points p1 and p2
     if k equals 1, it is a Manhattan (cityblock) distance
     if k equals 2, it is a Euclidean distance
     '''
-    if (type(p1) == np.ndarray) and (type(p2) == np.ndarray):
-        return np.power(np.sum(abs((p1-p2))**k), 1./k)
+    if  isinstance(p1, np.ndarray) and isinstance(p2, np.ndarray):
+        return np.power(np.sum(np.abs((p1-p2)).T**k, 0), 1./k) # do axis sum
+                                        # to allow for vectorized input
     else:
         x = map(lambda x,y: abs((x-y)**k), p1, p2)
         return np.power(np.sum(x), 1./k)
 
 def euclidean(p1,p2):
     "Euclidean distanc between 2 points"
-    return minkowsky(p1,p2,2)
+    return minkowski(p1,p2,2)
 
 def cityblock(p1,p2):
     "Cityblock (Manhattan) distance between 2 points"
-    return minkowsky(p1,p2,1)
+    return minkowski(p1,p2,1)
 
 def pearson(v1,v2):
     "Pearson distance measure"
--- imfun/som.py
+++ imfun/som.py
@@ -28,6 +28,7 @@ distance_fns = {
 def _sorted_affs(affs):
     return np.array(sorted(range(int(np.max(affs)+1)), key=lambda i:np.sum(affs==i)))
 
+
 def _iterative_som(patterns, max_rec=50, *args, **kwargs):
     affs_p, gr_p = som1(patterns, *args, output='both',  **kwargs)
     hist = []
@@ -45,15 +46,120 @@ def _iterative_som(patterns, max_rec=50, *args, **kwargs):
     return affs_n, gr_n
 
 
-def som1(patterns, shape=(10,1), alpha=0.99, r=2.0, neighbor_fn=neigh_gauss,
+def som1(patterns, gridshape=(10,1), alpha=0.99, r=2.0,
+         neighbor_fn=neigh_gauss,
          fade_coeff = 0.9,
          min_reassign=10,
          max_iter = 1e5,
          distance=euclidean,
 	 init_templates = None,
 	 init_pca = False,
+         random_query = True,
 	 output = 'last',
          verbose = 0):
+    """SOM as described in Bacao, Lobo and Painho, 2005
+    Parameters:
+      - `patterns` -- list or array-like, input patterns to train SOM against
+      - `gridshape` -- shape of SOM grid
+      - `alpha` -- \alpha parameter of SOM, "driving" force to adjust
+         neighboring nodes
+      - `r` -- radius for a neighbor function
+      - `neighbor_fn` -- a function to define neighborhood
+      - `fade_coeff` -- fading coefficient, parameters alpha and r are
+         multiplied at each step
+      - `min_reassign` -- stop after number of pattern reassignement has
+         reached this value
+      - `max_iter` -- don't do more than this number of iterations
+      - `distance` -- distance function (Euclidean distance by default)
+      - `init_templates` -- initialize SOM grid with this
+      - `init_pca` -- if init_templates is None, initialize templates as
+         first N principal components
+      - `output` - string to define output, can be 'last', 'both' or 'full'
+      - `verbose` - whether to be verboze 
+ 	 
+    """
+
+    if (type(distance) is str) and distance_fns.has_key(distance):
+        distance = distance_fns[distance]
+
+    ### TODOs:
+    ### [ ] go through patterns in random order, return sorted affiliations
+    ### [X] use principal components as a start-off patterns
+    ### [-] allow for sorting of at least 1D-grids
+    niter = 0
+    Npts = len(patterns)            # number of patterns
+    sh = patterns[0].shape          # dimensionality
+    grid = np.zeros(np.concatenate((gridshape, sh)))
+    locs = list(itt.product(*map(xrange,gridshape)))
+    L = len(locs)            # total dictionary size
+
+    if init_templates is None:
+	if init_pca:
+	    patt = np.array([x.ravel() for x in patterns])
+	    u,s,vh = np.linalg.svd(patt.T,full_matrices=False)
+	    init_templates = [c.reshape(sh) for c in u.T[:len(locs)]]
+	    del u,s,vh,patt
+	else:
+	    init_ks = np.random.randint(Npts, size=len(locs))
+	    init_templates = [patterns[k] for k in init_ks]
+    for k,l in enumerate(locs):
+        grid[l] = init_templates[k]
+
+    # initialize affiliations from patterns
+    affiliations = np.array([classify(p, grid, distance)[0] for p in patterns])
+    reassigned = len(affiliations)
+
+    out = []
+    grid_pdists = np.zeros((L,L))
+    flatgrid_sh = (-1, ) + sh
+    
+    while alpha > 1e-6 and niter < max_iter and reassigned > min_reassign:
+        affiliations_prev = affiliations.copy()
+        for k1,k2 in itt.combinations(range(L),2):
+            grid_pdists[k1,k2] = neighbor_fn(locs[k1],locs[k2],r)
+            grid_pdists[k2,k1] = grid_pdists[k1,k2]
+        for k,loc in enumerate(locs):
+            grid_pdists[k,k] = neighbor_fn(loc,loc,r)
+        ## Go through the patterns in random order
+        if random_query:
+            perm = np.random.permutation(Npts)
+        else:
+            perm = np.arange(Npts)
+        for k in perm:
+            p = patterns[k]
+            if (not k%100) and verbose:
+                sys.stderr.write("\r %04d %06d, %06d"%(niter, reassigned, Npts-k))
+            dists = distance(grid.reshape(flatgrid_sh), p)
+            winner_ind = np.argmin(dists)
+            affiliations[k] = winner_ind
+            update = (p-grid.reshape(flatgrid_sh))
+            aux_sh = (L,)+(1,)*len(sh)
+            update *= grid_pdists[winner_ind].reshape(aux_sh)
+            grid += alpha*update.reshape(grid.shape)
+        alpha *= fade_coeff
+        r *= fade_coeff
+        reassigned = np.sum(affiliations != affiliations_prev)
+        niter +=1
+	out.append(affiliations.copy())
+    if output == 'last':
+	return affiliations
+    elif output == 'grid':
+	return grid
+    elif output == 'both':
+	return affiliations, grid
+    elif output == 'full':
+        return out, grid
+    return out
+
+def _som1_old(patterns, shape=(10,1), alpha=0.99, r=2.0, neighbor_fn=neigh_gauss,
+              fade_coeff = 0.9,
+              min_reassign=10,
+              max_iter = 1e5,
+              distance=euclidean,
+              init_templates = None,
+              init_pca = False,
+              output = 'last',
+              verbose = 0):
     """SOM as described in Bacao, Lobo and Painho, 2005
     Parameters:
       - `patterns` -- list or array-like, input patterns to train SOM against
@@ -102,14 +208,14 @@ def som1(patterns, shape=(10,1), alpha=0.99, r=2.0, neighbor_fn=neigh_gauss,
         grid[l] = init_templates[k]
     #affiliations = np.ones(Npts)*-1
     # initialize affiliations from patterns
-    affiliations = np.array([classify(p, grid, distance)[0] for p in patterns])
+    affiliations = np.array([_classify_old(p, grid, distance)[0] for p in patterns])
     reassigned = len(affiliations)
     out = []
     while alpha > 1e-6 and niter < max_iter and reassigned > min_reassign:
         affiliations_prev = affiliations.copy()
         for k,p in enumerate(patterns):
             if (not k%100) and verbose:
-                sys.stderr.write("%04d %06d, %06d \r"%(niter, reassigned, Npts-k))
+                sys.stderr.write("\r %04d %06d, %06d"%(niter, reassigned, Npts-k))
             dists = [distance(grid[loc],p) for loc in locs]
             winner_ind = np.argmin(dists)
             affiliations[k] = winner_ind
@@ -130,6 +236,15 @@ def som1(patterns, shape=(10,1), alpha=0.99, r=2.0, neighbor_fn=neigh_gauss,
     return out
 
 def classify(pattern, grid, distance = euclidean):
+    sh = grid.shape[:2] # shape of grid
+    locs = list(itt.product(*map(xrange,sh)))
+    flatgrid_sh = (-1, ) + pattern.shape
+    dists = distance(grid.reshape(flatgrid_sh), pattern)
+    k = np.argmin(dists)
+    return k, locs[k]
+
+
+def _classify_old(pattern, grid, distance = euclidean):
     sh = grid.shape[:2] # shape of grid
     locs = list(itt.product(*map(xrange,sh)))
     dists = [distance(grid[loc],pattern) for loc in locs]
