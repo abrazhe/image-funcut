2db9d6be80de5ca6a7345cbc8dc4852f978d1958
Alexey Brazhe <brazhe@gmail.com>
2014-11-09 14:20:40 +0300

    changed version to 0.1.x

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='image-funcut',
-      version = '0.1.0',
+      version = '0.1.x',
       scripts = ['imfun/frame_viewer.py'],
       requires = ['numpy','scipy','swan'],
       py_modules = ['imfun.atrous',
