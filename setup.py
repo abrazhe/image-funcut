9f460a103d616922ff58b8b63d56792617652eb3
Alexey Brazhe <alexeybrazhe@yandex.ru>
2015-05-07 13:16:05 +0300

    micro-version bumped

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='image-funcut',
-      version = '0.1.2',
+      version = '0.1.3',
       description = "View, analyse and transform dynamic imaging data",
       scripts = ['imfun/frame_viewer.py'],
       requires = ['numpy','scipy','swan'],
