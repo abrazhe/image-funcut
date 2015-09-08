217959d91de3f271006328321d4a76216afaa385
Alexey Brazhe <alexeybrazhe@yandex.ru>
2015-09-08 19:30:04 +0200

    minor cleanups

--- imfun/lib.py
+++ imfun/lib.py
@@ -86,16 +86,6 @@ try:
 except:
     _scipyp = False
     
-def vessel_mask(f, p, negmask, thresh = None):
-	sh = f.shape
-	X,Y = meshgrid(*map(range, sh))
-	fx = f - plane(p,X,Y)
-	if thresh is None:
-		posmask = fx > median(fx) + fx.std()
-	else:
-		posmask = fx > thresh
-	return posmask*negmask
-	
 def in_circle(coords, radius):
     return lambda x,y: (square_distance((x,y), coords) <= radius**2)
 
@@ -145,9 +135,6 @@ def zero_low_sd(mat, n = 1.5):
 
 def arr_or(a1,a2):
     return np.vectorize(lambda x,y: x or y)(a1,a2)
-
-def shorten_movie(m,n):
-    return np.array([mean(m[i:i+n,:],0) for i in xrange(0, len(m), n)])
 
 def ma2d(m, n):
     "Moving average in 2d (for rows)"
@@ -204,7 +191,7 @@ def __best (scoref, lst):
     else: return -1,None
 
 def __min1(scoref, lst):
-    return best(lambda x,y: x < y, map(scoref, lst))
+    return __best(lambda x,y: x < y, map(scoref, lst))
 
 def allpairs(seq):
     return combinations(seq,2)
@@ -320,7 +307,7 @@ def DFoF(vec, normL=None, th = 1e-6):
 
 def clip_and_rescale(arr,nout=100):
     "convert data to floats in 0...1, throwing out nout max values"
-    out = arr - np.min(arr)
+    #out = arr - np.min(arr)
     cutoff = 100*(1-float(nout)/np.prod(arr.shape))
     m = np.percentile(arr, cutoff)
     return np.where(arr < m, arr, m)/m
@@ -540,27 +527,6 @@ def alias_freq(f, fs):
 
 ###---------- End Wavelet-related -------------	
 
-
-### Stackless trampolining
-def trampoline(function, *args):
-    """Bounces a function over and over, until we "land" off the
-    trampoline."""
-    bouncer = bounce(function, *args)
-    while True:
-        bouncer = bouncer[1](*bouncer[2])
-        if bouncer[0] == 'land':
-            return bouncer[1]
-
-
-def bounce(function, *args):
-    """Bounce back onto the trampoline, with an upcoming function call."""
-    return ["bounce", function, args]
-
-
-def land(value):
-    """Jump off the trampoline, and land with a value."""
-    return ["land", value]
-### --- end of stackless trampolining
 
 
 ## This is for reading Leica txt files
