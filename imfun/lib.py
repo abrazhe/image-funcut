3a139d3041570f39cfd98b2b7eab33aca101c8c3
Alexey Brazhe <alexeybrazhe@yandex.ru>
2016-09-16 11:01:46 +0200

    locextr with LSQ splines now

--- imfun/lib.py
+++ imfun/lib.py
@@ -400,12 +400,37 @@ def locextr(v, x=None, refine = True, output='full',
            return zip(xfit[maxima], yfit[maxima])
        elif output =='min':
            return zip(xfit[minima], yfit[minima])
-	
+
+def locextr_lsq_splines(v, x=None, points_per_knot=5,
+                        refine=True, output='full',sort_values=True):
+    from scipy.interpolate import LSQUnivariateSpline
+    if x is None: x = np.arange(len(v))
+    t  = np.linspace(x[2],x[-2],len(v)/points_per_knot)
+    spl = LSQUnivariateSpline(x,v,t)
+    if refine:
+        xfit = np.linspace(x[0],x[-1], len(x)*10)
+    else:
+        xfit = x
+    yfit = spl(xfit)
+    der1 = spl.derivative()(xfit)
+    dersign = np.sign(der1)
+
+    maxima = np.where(np.diff(dersign) < 0)[0]
+    minima = np.where(np.diff(dersign) > 0)[0]
+    if sort_values:
+        maxima = sorted(maxima, key=lambda p: yfit[p], reverse=True)
+        minima = sorted(minima, key=lambda p: yfit[p], reverse=False)
+    if output=='full':
+        return xfit, yfit, der1, maxima, minima 
+    elif output=='max':
+        return zip(xfit[maxima], yfit[maxima])
+    elif output =='min':
+        return zip(xfit[minima], yfit[minima])
 
 def extrema2(v, *args, **kwargs):
    "First and second order extrema"
-   xfit,yfit,der1,maxima,minima = locextr(v, *args, **kwargs)
-   xfit, _, der2, gups, gdowns = locextr(der1, x=xfit, refine=False)
+   xfit,yfit,der1,maxima,minima = locextr_lsq_splines(v, *args, **kwargs)
+   xfit, _, der2, gups, gdowns = locextr_lsq_splines(der1, x=xfit, refine=False)
    return (xfit, yfit), (maxima, minima), (gups, gdowns)
 
 def vinterpolate(v,n=3,smoothing=1):
@@ -468,7 +493,7 @@ def group_maps(maplist, ncols=None,
 	       suptitle = None,
 	       background = None,
 	       individual_colorbars = False,
-	       single_colorbar = None,
+	       colorbar = None,
 	       show_ticks = False,
 	       samerange = True,
 	       imkw=None, cbkw ={}):
@@ -490,11 +515,11 @@ def group_maps(maplist, ncols=None,
     if samerange:
 	vmin,vmax = data_range(maplist)
 	imkw.update(dict(vmin=vmin, vmax=vmax))
-	if single_colorbar is None:
-	    single_colorbar = True
+	if colorbar is None:
+	    colorbar = True
     else:
-	if single_colorbar is None:
-	    single_colorbar=False
+	if colorbar is None:
+	    colorbar=False
     if not imkw.has_key('aspect'):
 	imkw['aspect'] = 'equal'
     for i,f in enumerate(maplist):
@@ -508,7 +533,7 @@ def group_maps(maplist, ncols=None,
 	if individual_colorbars:
 	    figh.colorbar(im, ax=ax);
 	if titles is not None: pl.title(titles[i])
-    if single_colorbar:
+    if colorbar:
 	pl.subplots_adjust(bottom=0.1, top=0.9, right=0.8)
 	cax = pl.axes([0.85, 0.1, 0.03, 0.618])
 	pl.colorbar(im, cax=cax, **cbkw)
