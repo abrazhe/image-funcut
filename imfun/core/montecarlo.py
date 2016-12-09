2550f0d6983f64149414add95cdb86d70aa1ee44
Alexey Brazhe <alexeybrazhe@yandex.ru>
2016-12-09 22:13:16 +0300

    save confidence level and a stub for different noise models to use

--- imfun/core/montecarlo.py
+++ imfun/core/montecarlo.py
@@ -2,6 +2,9 @@
 
 import numpy as np
 import sys
+
+from .random_proc import ar1
+from .fnutils import take
  
 def mc_levels(transform_fn, size=(256,256),level=3, N = 1e3):
     """Return Monte-Carlo estimation of noise :math:`\\sigma`
@@ -27,7 +30,7 @@ def mc_levels(transform_fn, size=(256,256),level=3, N = 1e3):
        out[n] = map(np.std, transform_fn(im, level)[:-1])
     return np.mean(out, axis=0)
 
-def _mc_levels1d(transform_fn, size=1e5, level=12, N = 1e3):
+def _mc_levels1d(transform_fn, size=1e5, level=12, N = 1e3, noise_model='white'):
     """Return Monte-Carlo estimation of noise :math:`\\sigma`
 
     Parameters:
@@ -40,7 +43,12 @@ def _mc_levels1d(transform_fn, size=1e5, level=12, N = 1e3):
       - 1 :math:`\\times` level vector of noise :math:`\\sigma` estimations
     """
     import sys
-    signals = (np.random.randn(size) for i in np.arange(N))
+    if noise_model == 'white':
+        signals = (np.random.randn(size) for i in np.arange(N))
+    elif noise_model == 'ar1':
+        signals = (np.array(take(size, ar1())) for i in np.arange(N))
+    elif noise_model == 'ma':
+        signals = (np.mavg(np.random.randn(size+5),5)[:L] for i in np.arange(N))
     out  = np.zeros((N,level))
     for n,im in enumerate(signals):
 	x = np.mean(out[:n], axis=0)
@@ -48,5 +56,6 @@ def _mc_levels1d(transform_fn, size=1e5, level=12, N = 1e3):
 	s = '\r signal {:06d} out of {:06d}, current: {}'.format(n+1,long(N), s0)
 	sys.stderr.write(s)
 	#out[n] = map(np.std, decompose1d_direct(im, level)[:-1])
-        out[n] = map(np.std, transform_fn(im, level)[:-1])
+        #out[n] = map(np.std, transform_fn(im, level)[:-1])
+        out[n] = map(lambda v_: np.percentile(v_, 95), transform_fn(im, level)[:-1])
     return np.mean(out, axis=0)
