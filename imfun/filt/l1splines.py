48d9916ee2f54333d877ff9a52910c1fbe9f782f
Alexey Brazhe <alexeybrazhe@yandex.ru>
2016-12-20 12:54:56 +0300

    fixed imports from numpy

--- imfun/filt/l1splines.py
+++ imfun/filt/l1splines.py
@@ -1,6 +1,10 @@
 from __future__ import division
 
 import numpy as np
+
+from numpy import cos, pi, arange, ones, zeros
+from numpy.linalg import norm
+
 from scipy.fftpack import dct,idct
 from scipy import sparse
 
@@ -9,7 +13,7 @@ from scipy import sparse
 def shrink(x,gamma):
     """(x/|x|)*max(|x|-gamma)"""
     ax = np.abs(x)
-    return np.where(x!=0, x*clip(ax-gamma,0,np.nan)/ax, 0)
+    return np.where(x!=0, x*np.clip(ax-gamma,0,np.nan)/ax, 0)
 
 def l1sp_scale_to_smooth(scale):
     return (scale**4)/1000.
@@ -21,13 +25,13 @@ def l1spline(y, s=25.0, lam=None, weights=None, eps=1e-3, Ni=1,  niter=1000,
     if s_is_scale:
         s = l1sp_scale_to_smooth(s)
     if weights is None:
-        weights = np.ones(n)
+        weights = ones(n)
     if lam is None:
         lam = min(s,1)
-    gamma = 1./(1. + s*(-2. + 2.*cos((np.arange(n)*pi/n)))**2)
-    d = np.zeros(n)
-    b = np.zeros(n)
-    zprev = np.zeros(n)
+    gamma = 1./(1. + s*(-2. + 2.*cos((arange(n)*pi/n)))**2)
+    d = zeros(n)
+    b = zeros(n)
+    zprev = zeros(n)
     #acc = []
     for _i in range(niter):
         z = idct(gamma*dct((d+y-b),norm='ortho'),norm='ortho')
@@ -40,7 +44,7 @@ def l1spline(y, s=25.0, lam=None, weights=None, eps=1e-3, Ni=1,  niter=1000,
                 if verbose:
                     print 'converged after',_i,'iterations'
                 break
-        zprev = copy(z)
+        zprev = np.copy(z)
     return z#, acc
 
 
