fixing units as numpy recarrays

imfun/core/units.py
@@ -32,7 +32,7 @@ def quantities_to_scales(quants):
 def alist_to_scale(alist):
     while len(alist) < 3:
         alist.append(alist[-1])
-    return array(alist, dtype=dict(names=names,formats=formats)).view(np.recarray)
+    return np.array(alist, dtype=dict(names=names,formats=formats)).view(np.recarray)
 
 def scales_to_quantities(scales):
     return [Q(s['value'],s['unit'].astype(str)) for s in scales]

imfun/io/mes.py
@@ -8,7 +8,9 @@ from scipy import io
 
 
 from imfun.external.physics import Q
-from imfun.core import fnutils as fu
+from ..core.units import QS
+from ..core import units
+from ..core import fnutils as fu
 from ..core import array_handling as ah
 
 def guess_format(file_name):
@@ -205,7 +207,8 @@ class ZStack:
         #if ch is None: ch = self.ch
         #outmeta = {'ch':ch}
         outmeta = dict(ch=ch, timestamp=self.timestamps[0])
-        outmeta['axes'] = (Q(self.dz, 'um'),Q(self.dx, 'um'), Q(self.dx, 'um'))
+        #outmeta['axes'] = (Q(self.dz, 'um'),Q(self.dx, 'um'), Q(self.dx, 'um'))
+        outmeta['axes'] = units.alist_to_scale(((self.dz, 'um'),(self.dx, 'um'), (self.dx, 'um')))
         var_names = self.img_names
         nchannels = self.nchannels
 
@@ -267,8 +270,8 @@ class Timelapse:
     def load_data(self,ch):
         #if ch == None: ch = self.ch
         outmeta = dict(ch=ch, timestamp=self.timestamps[0])
-        #outmeta['axes'] = ah.alist_to_scale([(self.dt,'s'), (self.dx, 'um')])
-        outmeta['axes'] = [Q(self.dt, 's'), Q(self.dx, 'um'), Q(self.dx, 'um')]
+        outmeta['axes'] = units.alist_to_scale([(self.dt,'s'), (self.dx, 'um')])
+        #outmeta['axes'] = [Q(self.dt, 's'), Q(self.dx, 'um'), Q(self.dx, 'um')]
         nframes, nlines, line_len = self.nframes, self.nlines, self.line_length
         base_shape = list(map(np.int, (nframes-1, nlines, line_len)))
         streams = self._load_streams(ch=ch)

imfun/io/olympus.py
@@ -1,6 +1,10 @@
 import re
 import numpy as np
 from imfun.external.physics import Q
+
+from imfun.core import units
+from imfun.core.units import QS
+
 def parse_property(line):
     l = line.strip().replace('"','')
     name, value = l.split('\t')
@@ -29,14 +33,14 @@ def parse_dimension_entry(name, values):
     npoints = int(np_s)
     x = int_s.split()
     units = from_brackets(int_s)
-    #span = pair_to_scale((float(x[2])-float(x[0]),units))
-    span = Q(float(x[2])-float(x[0]),units)
+    span = QS(float(x[2])-float(x[0]),units)
+    #span = Q(float(x[2])-float(x[0]),units)
     units =  from_brackets(sampling_s)
     if units:
         units = units.split('/')[0]
         s = sampling_s.split()[0]
         #sampling  = pair_to_scale((s,units))
-        sampling = Q(float(s), units)
+        sampling = QS(float(s), units)
     else:
         sampling = sampling_s
     return name, (npoints, span, sampling)
@@ -64,5 +68,6 @@ def dimensions_to_axes(md):
     order = ('TZ','Y','X')
     keys = [[k for k in keys if k[0] in o][0] for o in order]
     values = [md[k] for k in keys]
-    out = [isinstance(v[-1],Q) and v[-1] or v[1]/v[0] for v in values]
+    #out = [isinstance(v[-1],Q) and v[-1] or v[1]/v[0] for v in values]
+    out = [QS(v[1].value/v[0],v[1].unit) if isinstance(v[-1],str) else v[-1] for v in values]
     return np.array(out)

imfun/ui/Picker_.py
@@ -922,7 +922,7 @@ class Picker (object):
         return
 
     def show_zview(self, rois = None, **kwargs):
-        print('in Picker.show_zview()')
+        #print('in Picker.show_zview()')
 
         tx = self.active_stack.frame_idx()
         t0 = tx[self.frame_index]
@@ -939,7 +939,7 @@ class Picker (object):
 
             prefs = self.roi_prefixes(roi_tgroup)
             if roi_type is 'area':
-                print(prefs)
+                #print(prefs)
                 _sh = self.roi_objs[roi_tgroup[0]].get_zview().shape
 
                 fig, axs = plt.subplots(len(prefs),len(_sh) > 1 and _sh[1] or 1, squeeze=False)
@@ -1124,7 +1124,7 @@ class Picker (object):
         ax.set_xlabel("Frequency, Hz")
 
     def default_freqs(self, nfreqs = 1024):
-        dz = self.active_stack.meta['axes'][0].value
+        dz,zu = self.active_stack.meta['axes'][0]
         return np.linspace(4.0/(self.length()*dz),
                            0.5/dz, num=nfreqs)
 