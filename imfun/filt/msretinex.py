f5ced2a3c276c2526a84019f2f9965a7708d7d67
Alexey Brazhe <alexeybrazhe@yandex.ru>
2018-11-08 14:40:08 +0300

    add msretinex

---
+++ imfun/filt/msretinex.py
@@ -0,0 +1,10 @@
+from scipy import ndimage
+from .dctsplines import l2spline,l1spline
+
+def grayscale_msr(img, sigmas=(15,80,250),smoother=l2spline):
+    out = np.zeros(img.shape,np.float32)
+    img = img.astype(np.float32)
+    for s in sigmas:
+        smoothed =
+        out += np.log(img/(smoother(img,s)+0.001))
+    return out/len(sigmas)
